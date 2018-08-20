7b19b338043055870be257b0b0412d41d940edc2
mcflugen <mcflugen@gmail.com>
2018-08-19 22:06:33 -0600

    Remove pymt dependency.

--- pymt_child/child.py
+++ pymt_child/child.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import
 
-from pymt.framework.bmi_bridge import bmi_factory
+# from pymt.framework.bmi_bridge import bmi_factory
 from ._child import Child
 
 
 
-Child = bmi_factory(Child)
+# Child = bmi_factory(Child)
 
 del bmi_factory
