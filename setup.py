e85aeb644504ca81503842d735cf71ed49bc6d6b
mcflugen <mcflugen@gmail.com>
2020-10-19 13:40:28 -0600

    Back to development: 0.2.1

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     author_email="eric.hutton@gmail.com",
     description="PyMT plugin for pymt_child",
     long_description=long_description,
-    version="0.2.0",
+    version="0.2.1.dev0",
     url="https://github.com/mcflugen/pymt_child",
     classifiers=[
         "Development Status :: 4 - Beta",
